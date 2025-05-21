3ea23a0591a1416afe0bd2210b66f1b1b6e8493e
akramiga <akramlubega73@gmail.com>
2025-05-21 15:29:30 +0300

    add template and static files

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 # Create your views here.
 def post_list(request):
     posts= Post.published.all()
-    return render(request, 'blog/posts/list.html', {'posts': posts} )
+    return render(request, 'blog/post/list.html', {'posts': posts} )
 
 def post_detail(request, id): 
     
