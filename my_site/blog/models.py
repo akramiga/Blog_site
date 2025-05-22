219ac6fe96d63257e72a763ae7fe5f07fc9fc273
akramiga <akramlubega73@gmail.com>
2025-05-22 17:02:30 +0300

    feat( add pagination and canonical urls)

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -11,7 +12,7 @@ class Post(models.Model):
         DRAFT= 'DF', 'Draft' 
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -25,3 +26,6 @@ class Post(models.Model):
         indexes =[models.Index(fields=['-publish'])]
     def __str__(self):
         return self.title 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+self.publish.month, self.publish.day, self.slug])
